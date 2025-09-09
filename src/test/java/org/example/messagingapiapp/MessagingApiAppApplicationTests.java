764f7a8f563181cb9eab46a91bc5f740e1f9e73e
Surya <surya.is21@bmsce.ac.in>
2025-09-09 14:40:20 +0530

    Creating messagin-api-app repo (OFSS)

---
+++ src/test/java/org/example/messagingapiapp/MessagingApiAppApplicationTests.java
@@ -0,0 +1,17 @@
+package org.example.messagingapiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class MessagingApiAppApplicationTests {
+
+    @Test
+    public void printGreetings()
+    {
+        assertTrue(false);
+    }
+
+}
